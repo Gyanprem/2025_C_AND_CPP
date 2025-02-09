0a982b46528887f1a2cf485f2a7fe41e540cb766
Gyan <gyan.chandra131@gmail.com>
2025-02-09 23:18:11 +0530

    update

---
+++ Device driver/3day/demo03/demo.c
@@ -0,0 +1,33 @@
+#include<linux/module.h>
+
+static __init int desd_init(void)
+{
+	printk(KERN_INFO "%s : desd_init() : state = %d\n", THIS_MODULE->name, THIS_MODULE->state);
+	printk(KERN_INFO "%s : desd_init() : initialization is successful\n", THIS_MODULE->name);
+	return 0;
+}
+
+static __exit void desd_exit(void)
+{
+	printk(KERN_INFO "%s : desd_exit() : state = %d\n", THIS_MODULE->name, THIS_MODULE->state);
+	printk(KERN_INFO "%s : desd_exit() : deinitialization is successful\n", THIS_MODULE->name);
+}
+
+module_init(desd_init);
+module_exit(desd_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Devendra Dhande");
+MODULE_DESCRIPTION("This is revision module");
+
+
+
+
+
+
+
+
+
+
+
+
